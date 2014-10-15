9fcb39340a049cf8dbd6de4b67de3abd5e65d5ae
kboy101222 <george.kyle87@gmail.com>
2014-10-15 13:19:37 -0500

    Added jQuery and Bootstrap

---
+++ index.php
@@ -0,0 +1,14 @@
+<header>
+  <script type='text/javascript' src='main.js'></script>
+  <script type='text/javascript' src='js/jquery.js'></script>
+  <link rel='stylesheet' src='css/bootstrap.css' />
+  <script type='text/javascript' src='js/bootstrap.js'></script>
+</header>
+
+<body>
+  <div class='block'>
+    <div class='btn-group'>
+      <button class='btn'>Home</button>
+    </div>
+  </div>
+</body>
